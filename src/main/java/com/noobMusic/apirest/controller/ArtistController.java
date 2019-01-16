fe0fd9f9b86c081f70b9d002c1a1bbafe7d727fa
IPICGI\IPI <steven.jolivald@ipilyon.net>
2019-01-16 17:56:29 +0100

    Exercice 2 et 3 validé

--- src/main/java/com/noobMusic/apirest/controller/ArtistController.java
+++ src/main/java/com/noobMusic/apirest/controller/ArtistController.java
@@ -3,10 +3,10 @@ package com.noobMusic.apirest.controller;
 import com.noobMusic.apirest.model.Artist;
 import com.noobMusic.apirest.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/artists")
@@ -19,8 +19,26 @@ public class ArtistController {
             method = RequestMethod.GET,
             value = "/{id}")
     public Artist seeArtist(
-        @PathVariable Integer id){
+            @PathVariable Integer id){
         return this.artistService.seeArtist(id);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            params = "name")
+    public List<Artist> seeArtistName(
+            @RequestParam ("name") String name){
+        return this.artistService.seeArtistName(name);
+    }
+
+
+    @RequestMapping
+    public Page<Artist> findAllArtist
+            (@RequestParam(value="page") Integer page,
+             @RequestParam(value="size") Integer size,
+             @RequestParam(value="sortProperty") String sortProperty,
+             @RequestParam(value="sortDirection") String sortDirection) {
+        return artistService.findAllArtist(page, size, sortProperty, sortDirection);
     }
 
 }
--- src/main/java/com/noobMusic/apirest/service/ArtistService.java
+++ src/main/java/com/noobMusic/apirest/service/ArtistService.java
@@ -3,16 +3,29 @@ package com.noobMusic.apirest.service;
 import com.noobMusic.apirest.model.Artist;
 import com.noobMusic.apirest.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
 public class ArtistService {
 
+    public static final Integer PAGE_SIZE_MIN = 10;
+    public static final Integer PAGE_SIZE_MAX = 100;
+    public static final Integer PAGE_MIN = 0;
+
     @Autowired
     private ArtistRepository artistRepository;
+
+    public Long countArtist() {
+        return artistRepository.count();
+    }
 
     public Artist seeArtist (Integer id) {
         Artist a = artistRepository.findById(id);
@@ -20,5 +33,47 @@ public class ArtistService {
             throw new EntityNotFoundException("Impossible de trouver l'artiste n° : " + id);
         }
         return a;
+    }
+
+    public List<Artist> seeArtistName(String name) {
+        List<Artist> artist = artistRepository.findByName(name);
+        if (artist == null){
+            throw new EntityNotFoundException("L'artiste de nom" + name + " n'a pas été trouvé");
+        }
+        return artist;
+    }
+
+    public Page<Artist> findAllArtist(Integer page, Integer size, String sortProperty, String sortDirection) {
+
+        if (page == null) {
+            page = PAGE_MIN;
+        } else if(page < 0) {
+            throw new IllegalArgumentException("Le numéro de page ne peut être inférieur à 0");
+        }
+
+        if (size == null) {
+            size = PAGE_SIZE_MIN;
+        } else if(size < 0 || size > PAGE_SIZE_MAX) {
+            throw new IllegalArgumentException("La taille de la page doit être comprise entre 1 et " +  (countArtist() / (page+1)));
+        } else if (page > countArtist()/size) {
+            throw new IllegalArgumentException("Le numéro de page ne peut être sup à " + (countArtist() / size));
+        }
+
+        Sort sort = null;
+        try {
+            sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection),sortProperty));
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Le sens du tri peut valoir 'ASC' pour un tri ascendant ou 'DESC' pour un tri descendant (insensible à la casse");
+        }
+        Pageable pageable = new PageRequest(page,size,sort);
+        Page<Artist> artists = artistRepository.findAll(pageable);
+        if(page >= artists.getTotalPages()){
+            throw new IllegalArgumentException("Le numéro de page ne peut être supérieur à " + artists.getTotalPages());
+        } else if(artists.getTotalElements() == 0){
+            throw new EntityNotFoundException("Il n'y a aucun artist dans la base de données");
+        }
+
+        return artistRepository.findAll(pageable);
     }
 }
